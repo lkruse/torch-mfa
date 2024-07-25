6f06f28f1a86e93fab3752ebff1690d196f54bc6
Liam Kruse <liam.a.kruse@boeing.com>
2024-07-25 15:02:31 -0700

    Edits to MPPCA class

--- mppca.py
+++ mppca.py
@@ -5,6 +5,24 @@ from sklearn.cluster import KMeans
 class MPPCA(torch.nn.Module):
 
     def __init__(self, n_components, n_features, n_factors):
+        """
+        Initialize the Mixture of Probabilistic Principal Component Analyzers.
+
+        Original publication:
+        [1] Tipping, M. E., & Bishop, C. M. (1999). Mixtures of probabilistic 
+            principal component analyzers. Neural Computation, 11(2), 443-482.
+
+        The implementation is based on the open-source code from
+        [2] Richardson, E., & Weiss, Y. (2018). On GANs and GMMs. Advances in 
+            Neural Information Processing Systems, 31.
+
+        Parameters:
+            waypoints (list of np.array): list of waypoints as numpy arrays
+            stop_duration (float): duration of pallet pause at each waypoint (seconds)
+            max_speed (float): maximum speed of the pallet (meters per second)
+            acc (float): acceleration of the pallet (meters per second^2)
+            dt (float): simulation timestep (seconds)
+        """
         super(MPPCA, self).__init__()
         self.n_components = n_components
         self.n_features = n_features
@@ -32,10 +50,10 @@ class MPPCA(torch.nn.Module):
 
         samples = Wz + mu + epsilon
 
-        return samples, sampled_components
+        return samples.squeeze(), sampled_components
 
 
-    def _component_log_likelihood(self, x, pi, mu, W, sigma2):
+    def _component_log_likelihood(self, x, W, mu, pi, sigma2):
         K, d, l = W.shape
         WT = W.transpose(1,2)
         inv_sigma2 = (1.0/sigma2 * torch.ones(d,1)).view(d,K,1).transpose(0,1)
@@ -48,6 +66,7 @@ class MPPCA(torch.nn.Module):
             x_c = (x - mu[i].reshape(1,d)).T
             component_m_d = (inv_sigma2[i] * x_c) - \
                 ((inv_sigma2[i] * W[i]) @ inv_L[i]) @ (WT[i] @ (inv_sigma2[i] * x_c))
+            
             return torch.sum(x_c * component_m_d, dim=0)
 
         # combine likelihood terms
@@ -66,7 +85,9 @@ class MPPCA(torch.nn.Module):
         :param x: samples [n, n_features]
         :return: log-probability values [n]
         """
-        return torch.logsumexp(self._component_log_likelihood(x, self.pi, self.mu, self.W, self.sigma2), dim=1)
+        component_lls = self._component_log_likelihood(x, self.W, self.mu, self.pi, self.sigma2)
+
+        return torch.logsumexp(component_lls, dim=1)
     
 
     def responsibilities(self, x):
@@ -75,8 +96,8 @@ class MPPCA(torch.nn.Module):
         :param x: samples [n, n_features]
         :return: responsibility values [n, n_components]
         """
-        comp_LLs = self._component_log_likelihood(x, self.pi, self.mu, self.W, self.sigma2)
-        log_responsibilities = comp_LLs - self.log_prob(x).reshape(-1, 1)
+        component_lls = self._component_log_likelihood(x, self.W, self.mu, self.pi, self.sigma2)
+        log_responsibilities = component_lls - self.log_prob(x).reshape(-1, 1)
 
         return torch.exp(log_responsibilities)
 
@@ -88,69 +109,68 @@ class MPPCA(torch.nn.Module):
 
         V = V.T.to(x.device)
         S = S.to(x.device)
-        # (eq) 3.13
-        sigma_squared = torch.sum(torch.pow(S[n_factors:], 2.0))/((x.shape[0]-1) * (x.shape[1]-n_factors))
-        # (eq) 3.12, S in (eq) 3.9
-        A = V[:, :n_factors] * torch.sqrt((torch.pow(S[:n_factors], 2.0).reshape(1, n_factors)/(x.shape[0]-1) - sigma_squared))
-        return mu, A, sigma_squared#torch.log(sigma_squared) * torch.ones(x.shape[1], device=x.device)
+        # (3.13) in Tipping and Bishop (1999) [1]
+        sigma2 = torch.sum(S[n_factors:]**2.0)/((x.shape[0]-1) * (x.shape[1]-n_factors))
+        # (3.12) in Tipping and Bishop (1999) [1]
+        W = V[:, :n_factors] * torch.sqrt((S[:n_factors]**2.0).reshape(1, n_factors)/(x.shape[0]-1) - sigma2)
+
+        return W, mu, sigma2
 
 
     def _init_from_data(self, x):
-        n = x.shape[0]
         K, d, l = self.W.shape
-
         t = time.time()
-        print('Performing K-means clustering of {} samples in dimension {} to {} clusters...'.format(
-            x.shape[0], d, K))
-        _x = x.cpu().numpy()
-        clusters = KMeans(n_clusters=K, max_iter=300).fit(_x)
-        print('... took {} sec'.format(time.time() - t))
-        component_samples = [clusters.labels_ == i for i in range(K)]
+        print('Performing K-means clustering to {} clusters...'.format(K))
+        clusters = KMeans(n_clusters=K, max_iter=300).fit(x)
+        print('... took {:.4f} sec'.format(time.time() - t))
+        labels = [clusters.labels_ == i for i in range(K)]
 
         params = [torch.stack(t) for t in zip(
-            *[self._small_sample_ppca(x[component_samples[i]], n_factors=l) for i in range(K)])]
+            *[self._small_sample_ppca(x[labels[i]], n_factors=l) for i in range(K)])]
 
-        self.mu.data = params[0]
-        self.W.data = params[1]
-        #self.log_D.data = params[2]
-        self.sigma2.data = params[2]
+        self.W = params[0]
+        self.mu = params[1]
+        self.sigma2 = params[2]
 
 
-    def fit(self, x, max_iterations=20):
+    def fit(self, x, max_iterations=50):
         K, d, l = self.W.shape
         N = x.shape[0]
 
-        print('Random init...')
         self._init_from_data(x)
-        print('Init log-likelihood =', round(torch.mean(self.log_prob(x)).item(), 1))
+        print('Initial log-likelihood = {:.4f}'.format(torch.mean(self.log_prob(x)).item()))
 
+        # All equations and appendices reference Tipping and Bishop (1999) [1]
         def per_component_m_step(i):
-            mu_i = torch.sum(r[:, [i]] * x, dim=0) / r_sum[i]
-            s2_I = self.sigma2[i] * torch.eye(l, device=x.device)
-            inv_M_i = torch.inverse(self.W[i].T @ self.W[i] + s2_I)
-            x_c = x - mu_i.reshape(1, d)
-            SiAi = (1.0/r_sum[i]) * (r[:, [i]]*x_c).T @ (x_c @ self.W[i])
-            invM_AT_Si_Ai = inv_M_i @ self.W[i].T @ SiAi
-            A_i_new = SiAi @ torch.inverse(s2_I + invM_AT_Si_Ai)
-            t1 = torch.trace(A_i_new.T @ (SiAi @ inv_M_i))   # (eq) 6 in [2]
-            trace_S_i = torch.sum(N/r_sum[i] * torch.mean(r[:, [i]]*x_c*x_c, dim=0)) # (eq) 6 in [2]
-            sigma_2_new = (trace_S_i - t1)/d # (eq) 6 in [2]
-            return mu_i, A_i_new, sigma_2_new#torch.log(sigma_2_new) * torch.ones_like(self.log_D[i])
+            # (C.8)
+            mui_new = torch.sum(r[:, [i]] * x, dim=0) / r_sum[i]
+            sigma2_I = self.sigma2[i] * torch.eye(l, device=x.device)
+            inv_Mi = torch.inverse(self.W[i].T @ self.W[i] + sigma2_I)
+            x_c = x - mui_new.reshape(1, d)
+            # efficiently calculate (Si)(Wi) as discussed in Appendix C
+            SiWi = (1.0/r_sum[i]) * (r[:, [i]]*x_c).T @ (x_c @ self.W[i])
+            # (C.14)
+            Wi_new = SiWi @ torch.inverse(sigma2_I + inv_Mi @ self.W[i].T @ SiWi)
+            # (C.15)
+            t1 = torch.trace(Wi_new.T @ (SiWi @ inv_Mi))
+            trace_Si = torch.sum(N/r_sum[i] * torch.mean(r[:, [i]]*x_c*x_c, dim=0))
+            sigma_2_new = (trace_Si - t1)/d
 
+            return Wi_new, mui_new, sigma_2_new
 
-        ll_log = []
+        log_likelihoods = []
         for it in range(max_iterations):
             t = time.time()
             r = self.responsibilities(x)
             r_sum = torch.sum(r, dim=0)
             new_params = [torch.stack(t) for t in zip(*[per_component_m_step(i) for i in range(K)])]
-            self.mu.data = new_params[0]
-            self.W.data = new_params[1]
-            #self.log_D.data = new_params[2]
-            self.sigma2.data = new_params[2]
-            self.pi.data = r_sum / torch.sum(r_sum)
-            ll = round(torch.mean(self.log_prob(x)).item(), 5)
-            print('Iteration {}/{}, train log-likelihood = {}, took {:.1f} sec'.format(it, max_iterations, ll,
+            self.W = new_params[0]
+            self.mu = new_params[1]
+            self.sigma2 = new_params[2]
+            self.pi = r_sum / torch.sum(r_sum)
+            ll = torch.mean(self.log_prob(x)).item()
+            print('Iteration {}/{}, train log-likelihood = {:.4f}, took {:.4f} sec'.format(it, max_iterations, ll,
                                                                                    time.time()-t))
-            ll_log.append(ll)
-        return ll_log
+            log_likelihoods.append(ll)
+
+        return log_likelihoods
--- simple_test.py
+++ simple_test.py
@@ -2,21 +2,21 @@
 # packages
 import matplotlib.pyplot as plt
 import torch
+from mppca import MPPCA
+from mfa import MFA
+
 from torch.utils.data import DataLoader, Dataset
 
-from mfa import MFA
-from mppca import MPPCA
-
-n_samples = 1000
+n_samples = 100000
 n_features = 4
 n_latent = 2
 n_components = 2
-noise_variance = 0.1
+noise_variance = 0.01
 
 pastelBlue = "#0072B2"
 pastelRed = "#F5615C"
 
-def generate_mppca_data(n_samples=1000, n_features=6, n_latent=2, n_components=2, noise_variance=0.1, seed=0):
+def generate_test_data(n_samples=1000, n_features=6, n_latent=2, n_components=2, noise_variance=0.01, seed=0):
     torch.manual_seed(seed)  # for reproducibility
     # generate latent variables
     Z = torch.randn((n_samples, n_latent))
@@ -36,8 +36,24 @@ def generate_mppca_data(n_samples=1000, n_features=6, n_latent=2, n_components=2
         Z_k = Z[mask]
         if Z_k.shape[0] > 0:
             X_clean = torch.matmul(Z_k, W[k, ...]) + mu[k, :]
-            noise = torch.randn(Z_k.size(0), n_features) * noise_variance
+            noise = torch.randn(Z_k.size(0), n_features) * noise_variance**0.5
             X[mask] = X_clean + noise
+    '''
+    pi = torch.ones((n_components,)) / n_components
+    sampled_components = torch.multinomial(pi, n_samples, replacement=True)
+    z_l = torch.randn(n, l, device=self.W.device)
+
+    if with_noise:
+        z_d = torch.randn(n, d, device=self.W.device)  
+    else:
+        z_d = torch.zeros(n, d, device=self.W.device)
+
+    Wz = self.W[sampled_components] @ z_l[..., None]
+    mu = self.mu[sampled_components][..., None]
+    epsilon = (z_d * self.sigma2[sampled_components][..., None]**0.5)[..., None]
+
+    samples = Wz + mu + epsilon
+    '''
 
     return X
 
@@ -66,29 +82,56 @@ def plot_data(n_features, X, color=pastelBlue):
 
 #X = generate_mppca_data(n_samples, n_features, n_latent, n_components, noise_variance, seed = 10)
 #plot_data(n_features, X[:500], color=pastelBlue)
+# wrap the dataset in a PyTorch DataLoader
+class ToyDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = torch.tensor(data, dtype=torch.float32)
+        self.labels = torch.tensor(labels, dtype=torch.long)
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
 
+# Create a dataset and dataloader
+y = torch.randint(10, (n_samples,))
 
 #%%
-n_samples = 10000
-X_train = generate_mppca_data(n_samples, n_features, n_latent, n_components, noise_variance, seed = 2)
+
+X_train = generate_test_data(n_samples, n_features, n_latent, n_components, noise_variance, seed = 2)
 y_train = torch.randint(4, (n_samples,))
 
-X_test = generate_mppca_data(n_samples, n_features, n_latent, n_components, noise_variance, seed = 1)
+X_test = generate_test_data(n_samples, n_features, n_latent, n_components, noise_variance, seed = 1)
 y_test = torch.randint(4, (n_samples,))
 
 plot_data(n_features, X_train[:500], color=pastelBlue)
 plt.savefig("train.png")
 plt.show()
 
+n_features = 500
+n_latent = 10
+n_components = 42
+
+X_train = torch.randn((n_samples, n_features))
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-model = MPPCA(n_components=n_components, n_features=n_features, n_factors=n_latent).to(device)
-#model = MFA(n_components=n_components, n_features=n_features, n_factors=n_latent).to("cpu")
+#model = MPPCA(n_components=n_components, n_features=n_features, n_factors=n_latent).to(device)
+model = MFA(n_components=n_components, n_features=n_features, n_factors=n_latent).to("cpu")
 
 X_train.to(device)
 
-ll_log = model.fit(X_train, max_iterations=100)
+
+
+# Create a dataset and dataloader
+dataset = ToyDataset(X_train, y)
+dataloader = DataLoader(dataset, batch_size=500, shuffle=True, drop_last=True)
+
+
+
+#ll_log = model.batch_fit(X_train, max_iterations=100)
+ll_log = model.batch_fit(dataset, max_iterations=100, batch_size=1000)
 
 plt.plot(ll_log)
 plt.show()
@@ -123,5 +166,52 @@ sampled_X, _ = model.sample(500, with_noise=True)
 plot_data(n_features, sampled_X, color=pastelRed)
 
 plt.savefig("gen.png")
+plt.show()
+
+
+#%%
+################################################################################
+import json
+import matplotlib.pyplot as plt
+import torch
+
+from mppca import MPPCA
+
+pastelBlue = "#0072B2"
+pastelRed = "#F5615C"
+
+
+#%%
+with open('data/train_data.json', 'r') as f:
+    train_data = torch.tensor(json.load(f))
+with open('data/train_labels.json', 'r') as f:
+    train_labels = json.load(f)
+
+#with open('data/test_data.json', 'r') as f:
+#    test_data = torch.tensor(json.load(f))
+#with open('data/test_labels.json', 'r') as f:
+#    test_labels = json.load(f)
+#%%
+N, L, D = train_data.shape
+flat_train = train_data.view(N, L*D)
+
+# define constants
+n_features = L*D
+n_factors = 6
+n_components = 42
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+flat_train.to(device)
+
+model = MPPCA(n_components=n_components, n_features=n_features, n_factors=n_factors).to(device)
+
+ll_log = model.fit(flat_train, max_iterations=100)
+
+#X_samples, _ = model.sample(n_plot, with_noise=True)
+
+# plot log-likelihood
+plt.plot(ll_log, c = pastelBlue)
+plt.xlabel("EM iteration")
+plt.ylabel("Log-Likelihood")
 plt.show()
 # %%
